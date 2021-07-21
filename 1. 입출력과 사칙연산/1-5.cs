7986a2e6e9940a6b5ac1f637580c37b2005c2844
simplex3510 <leeyj6457@naver.com>
2021-07-21 18:39:45 +0900

    주석처리

--- 1. 입출력과 사칙연산/1-5.cs
+++ 1. 입출력과 사칙연산/1-5.cs
@@ -1,19 +1,19 @@
-using System;
+// using System;
 
-namespace backjoon
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string s = Console.ReadLine();
+// namespace backjoon
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             string s = Console.ReadLine();
 
-            string[] arr = s.Split(new string[] {" "}, StringSplitOptions.None);
+//             string[] arr = s.Split(new string[] {" "}, StringSplitOptions.None);
 
-            int a = Convert.ToInt32(arr[0]);
-            int b = Convert.ToInt32(arr[1]);
+//             int a = Convert.ToInt32(arr[0]);
+//             int b = Convert.ToInt32(arr[1]);
 
-            Console.WriteLine($"{a+b}");
-        }
-    }
-}
+//             Console.WriteLine($"{a+b}");
+//         }
+//     }
+// }
