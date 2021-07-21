b3020248ea219b762ded453f65dd83c0dafdbbbd
simplex3510 <leeyj6457@naver.com>
2021-07-22 01:17:13 +0900

    3단계 문제풀이

---
+++ 3. for문/3-1.cs
@@ -0,0 +1,16 @@
+// using System;
+
+// namespace backjoon2_1
+// {
+//     class MainApp
+//     {
+//         static void Main()
+//         {
+//             int num = Convert.ToInt32(Console.ReadLine());
+
+//             for (int i = 1; i < 10; i++) {
+//                 Console.WriteLine($"{num} * {i} = {num * i}");
+//             }
+//         }
+//     }
+// }
