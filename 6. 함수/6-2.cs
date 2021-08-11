abb52aa1e041ddec627684a7a523ed7cf987f10c
simplex3510 <leeyj6457@naver.com>
2021-08-11 18:28:50 +0900

    7장 연습문제 풀이

--- 6. 함수/6-2.cs
+++ 6. 함수/6-2.cs
@@ -1,39 +1,39 @@
-using System;
+// using System;
 
-namespace bacckjook6_2
-{
-    class MainApp
-    {
-        static void HanSu(int num)
-        {
-            int count = 0;
+// namespace bacckjook6_2
+// {
+//     class MainApp
+//     {
+//         static void HanSu(int num)
+//         {
+//             int count = 0;
 
-            if(num < 100) { count=num; }
+//             if(num < 100) { count=num; }
 
-            if (100 <= num)
-            {
-                count=99;
-                for (int j = 100; j <= num; j++)
-                {
-                    int num0 = j;
-                    int num1 = num0 / 100;
-                    int num1_1 = num0 % 100;
-                    int num2 = num1_1 / 10;
-                    int num3 = num1_1 % 10;
+//             if (100 <= num)
+//             {
+//                 count=99;
+//                 for (int j = 100; j <= num; j++)
+//                 {
+//                     int num0 = j;
+//                     int num1 = num0 / 100;
+//                     int num1_1 = num0 % 100;
+//                     int num2 = num1_1 / 10;
+//                     int num3 = num1_1 % 10;
 
-                    if ((num1 - num2) == (num2 - num3))
-                        count++;
-                }
-            }
+//                     if ((num1 - num2) == (num2 - num3))
+//                         count++;
+//                 }
+//             }
 
-            Console.WriteLine($"{count}");
-        }
+//             Console.WriteLine($"{count}");
+//         }
 
-        static void Main()
-        {
-            string str = Console.ReadLine();
-            int num = Convert.ToInt32(str);
-            HanSu(num);
-        }
-    }
-}
+//         static void Main()
+//         {
+//             string str = Console.ReadLine();
+//             int num = Convert.ToInt32(str);
+//             HanSu(num);
+//         }
+//     }
+// }
