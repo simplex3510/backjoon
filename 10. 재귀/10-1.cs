c67b2a12561c533ced2b318ec7285a8bd061dcbf
simplex3510 <leeyj6457@naver.com>
2021-08-31 12:42:43 +0900

    10단계 문제풀이

---
+++ 10. 재귀/10-1.cs
@@ -0,0 +1,23 @@
+// using System;
+
+// namespace baekjoon10_1
+// {
+//     class MainApp
+//     {
+//         static int factorial(int n)
+//         {
+//             if(n == 1)
+//                 return n;
+//             else if (n == 0)
+//                 return 1;
+
+//             return n * factorial(n-1);
+//         }
+
+//         static void Main()
+//         {
+//             int fac = Convert.ToInt32(Console.ReadLine());
+//             Console.WriteLine(factorial(fac));
+//         }
+//     }
+// }
---
+++ 10. 재귀/10-2.cs
@@ -0,0 +1,22 @@
+// using System;
+
+// namespace baekjoon10_2
+// {
+//     class MainApp
+//     {
+//         static int fibo(int n)
+//         {
+//             if(n == 0) return 0;
+//             else if (n == 1) return 1;
+
+//             return fibo(n-1) + fibo(n-2);
+//         }
+
+
+//         static void Main()
+//         {
+//             int n = Convert.ToInt32(Console.ReadLine());
+//             Console.WriteLine(fibo(n));
+//         }
+//     }
+// }
