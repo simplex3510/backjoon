99a775e618061b1e1abedf6efa825b0dd45e6725
simplex3510 <leeyj6457@naver.com>
2021-09-01 15:30:16 +0900

    10단계 연습문제 풀이(수정 필요)

---
+++ 10. 재귀/10-3.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Text;
+
+namespace baekjoon10_3
+{
+    class MainApp
+    {
+        static void star(char[,] matrix, int x, int y, int n)
+        {
+            if (n == 1)
+            {
+                matrix[y, x] = '*';
+                return;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i == 1 && j == 1)
+                        matrix[i, j] = ' ';
+                    else
+                        star(matrix, x + ( n / 3 * j), y + (n / 3 * i), n / 3);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int n = Convert.ToInt32(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            star(matrix, 0, 0, n);
+
+            // 출력
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    sb.Append(matrix[i, j]);
+                }
+                sb.Append('\n');
+            }
+            Console.Write(sb);
+        }
+    }
+}
