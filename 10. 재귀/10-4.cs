eff14419c395d2d6efe7fb0cd047ddd548f07f6b
simplex3510 <leeyj6457@naver.com>
2021-09-02 14:57:17 +0900

    10단계 문제풀이

--- 10. 재귀/10-4.cs
+++ 10. 재귀/10-4.cs
@@ -1,36 +1,36 @@
-using System;
-using System.Text;
+// using System;
+// using System.Text;
 
-namespace baekjoon10_2
-{
-    class MainApp
-    {
-        static StringBuilder sb = new StringBuilder();
-        static int count = 0;
-        static void hanoi(int n, char from, char temp, char to)
-        {
-            count++;
+// namespace baekjoon10_2
+// {
+//     class MainApp
+//     {
+//         static StringBuilder sb = new StringBuilder();
+//         static int count = 0;
+//         static void hanoi(int n, char from, char temp, char to)
+//         {
+//             count++;
 
-            if (n == 1)
-            {
-                sb.Append($"{from} {to}\n");
-            }
-            else
-            {
-                hanoi(n-1, from, to, temp);
-                sb.Append($"{from} {to}\n");
-                hanoi(n-1, temp, from, to);
-            }
-        }
+//             if (n == 1)
+//             {
+//                 sb.Append($"{from} {to}\n");
+//             }
+//             else
+//             {
+//                 hanoi(n-1, from, to, temp);
+//                 sb.Append($"{from} {to}\n");
+//                 hanoi(n-1, temp, from, to);
+//             }
+//         }
 
 
-        static void Main()
-        {
-            int n = Convert.ToInt32(Console.ReadLine());
-            hanoi(n, '1', '2', '3');
-            Console.WriteLine(count);
-            Console.WriteLine(sb);
+//         static void Main()
+//         {
+//             int n = Convert.ToInt32(Console.ReadLine());
+//             hanoi(n, '1', '2', '3');
+//             Console.WriteLine(count);
+//             Console.WriteLine(sb);
 
-        }
-    }
-}
+//         }
+//     }
+// }
