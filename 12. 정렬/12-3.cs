2de24c354cd8e9940cd57f85b277e8814e2d75e2
simplex3510 <leeyj6457@naver.com>
2021-09-07 14:40:16 +0900

    12단계 문제풀이

--- 12. 정렬/12-3.cs
+++ 12. 정렬/12-3.cs
@@ -1,49 +1,49 @@
-using System;
-using System.Text;
-using System.IO;
+// using System;
+// using System.Text;
+// using System.IO;
 
-namespace baekjoon12_3
-{
-    class MainApp
-    {
-        static void Main()
-        {
-            int[] counting = new int[10_001];
+// namespace baekjoon12_3
+// {
+//     class MainApp
+//     {
+//         static void Main()
+//         {
+//             int[] counting = new int[10_001];
 
-            //var sr = new StreamReader(Console.OpenStandardInput());
-            var sw = new StreamWriter(Console.OpenStandardOutput());
-            StringBuilder sb = new StringBuilder();
-            int testCase = int.Parse(Console.ReadLine());
-            int max = int.MinValue;
-            int num = 0;
+//             //var sr = new StreamReader(Console.OpenStandardInput());
+//             var sw = new StreamWriter(Console.OpenStandardOutput());
+//             StringBuilder sb = new StringBuilder();
+//             int testCase = int.Parse(Console.ReadLine());
+//             int max = int.MinValue;
+//             int num = 0;
 
-            for (int i = 0; i < testCase; i++)    // 카운팅
-            {
-                num = int.Parse(Console.ReadLine());
-                counting[num]++;
-                if (max < num)
-                    max = num;
-            }
+//             for (int i = 0; i < testCase; i++)    // 카운팅
+//             {
+//                 num = int.Parse(Console.ReadLine());
+//                 counting[num]++;
+//                 if (max < num)
+//                     max = num;
+//             }
 
 
-            for (int i = 0; i <= max;)             // 정렬해서 출력
-            {
-                if (counting[i] == 0)
-                {
-                    i++;
-                    continue;
-                }
+//             for (int i = 0; i <= max;)             // 정렬해서 출력
+//             {
+//                 if (counting[i] == 0)
+//                 {
+//                     i++;
+//                     continue;
+//                 }
 
-                sb.Append(i + "\n");
-                counting[i]--;
-                if (600 <= sb.Length)
-                {
-                    sw.Write(sb);
-                    sb.Clear();
-                }
-            }
-            sw.Write(sb);
-            sw.Close();
-        }
-    }
-}
+//                 sb.Append(i + "\n");
+//                 counting[i]--;
+//                 if (600 <= sb.Length)
+//                 {
+//                     sw.Write(sb);
+//                     sb.Clear();
+//                 }
+//             }
+//             sw.Write(sb);
+//             sw.Close();
+//         }
+//     }
+// }
