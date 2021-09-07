db6e4a22e7868ba4c78ea7c6d00faec2877d6da9
simplex3510 <leeyj6457@naver.com>
2021-09-07 14:39:59 +0900

    12단계 문제풀이

--- 12. 정렬/12-5.cs
+++ 12. 정렬/12-5.cs
@@ -1,45 +1,45 @@
-using System;
+// using System;
 
-namespace baekjoon12_5
-{
-    class MainApp
-    {
-        static int[] counting = new int[10_001];
+// namespace baekjoon12_5
+// {
+//     class MainApp
+//     {
+//         static int[] counting = new int[10_001];
 
-        static int[] CountingSort(int[] arr, int max)
-        {
-            int[] result = new int[arr.Length];
+//         static int[] CountingSort(int[] arr, int max)
+//         {
+//             int[] result = new int[arr.Length];
 
-            for (int i = 0; i < arr.Length; i++)    // 카운팅
-                counting[arr[i]]++;
+//             for (int i = 0; i < arr.Length; i++)    // 카운팅
+//                 counting[arr[i]]++;
 
-            for (int i = 1; i <= max; i++)          // 누적합 연산
-                counting[i] += counting[i - 1];
+//             for (int i = 1; i <= max; i++)          // 누적합 연산
+//                 counting[i] += counting[i - 1];
 
-            for (int i = 0; i < result.Length; i++) // 정렬
-                result[--counting[arr[i]]] = arr[i];
+//             for (int i = 0; i < result.Length; i++) // 정렬
+//                 result[--counting[arr[i]]] = arr[i];
 
-            return result;
-        }
+//             return result;
+//         }
 
-        static void Main()
-        {
-            char[] ch = Console.ReadLine().ToCharArray();
-            int[] arr = new int[ch.Length];
-            int max = int.MinValue;
+//         static void Main()
+//         {
+//             char[] ch = Console.ReadLine().ToCharArray();
+//             int[] arr = new int[ch.Length];
+//             int max = int.MinValue;
 
-            for (int i = 0; i < ch.Length; i++)
-            {
-                arr[i] = Convert.ToInt32(ch[i] - '0');
-                if (max < arr[i])
-                    max = arr[i];
-            }
+//             for (int i = 0; i < ch.Length; i++)
+//             {
+//                 arr[i] = Convert.ToInt32(ch[i] - '0');
+//                 if (max < arr[i])
+//                     max = arr[i];
+//             }
 
-            arr = CountingSort(arr, max);
-            Array.Reverse(arr);
+//             arr = CountingSort(arr, max);
+//             Array.Reverse(arr);
 
-            for (int i = 0; i < arr.Length; i++)
-                Console.Write(arr[i]);
-        }
-    }
-}
+//             for (int i = 0; i < arr.Length; i++)
+//                 Console.Write(arr[i]);
+//         }
+//     }
+// }
